26e096b4a745228bbe179ea90b4805284602471a
fernandaguzzo <guzzofernanda84@gmail.com>
2025-04-14 12:38:15 -0300

    adição de mais informações referentes ao livro

--- models/livro.py
+++ models/livro.py
@@ -5,14 +5,21 @@ livros = db.table('livros')
 LivroQuery = Query()
 
 class Livro:
-    def __init__(self, titulo, autor):
+    def __init__(self, titulo, autor, data_lancamento, sinopse):
         self.titulo = titulo
         self.autor = autor
+        self.data_lancamento = data_lancamento
+        self.sinopse = sinopse
+        self.edicao = edicao
 
     def salvar(self):
         livros.insert({
             'titulo': self.titulo,
-            'autor': self.autor
+            'autor': self.autor,
+            'data_lancamento' = data_lancamento
+            'sinopse' = sinopse
+            'edicao' = edicao
+
         })
 
     def buscar_por_termo(self):
